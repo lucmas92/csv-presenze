3e18b479653fe241949084efac98870b7408ac68
Luca Massignani <luca.massignani@sanmarcoinformatica.it>
2026-06-15 09:23:57 +0200

    aggiunto preset netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: true,
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+    ssr: true,
+    compatibilityDate: '2025-07-15',
+    devtools: {enabled: true},
+    modules: ['@nuxtjs/tailwindcss'],
+    nitro: {
+        preset: "netlify"
+    }
 })

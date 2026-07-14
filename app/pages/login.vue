815024c27aad1f24bc179f641e3be08b6521056f
Luca Massignani <luca.massignani@sanmarcoinformatica.it>
2026-07-14 09:21:14 +0200

    aggiunto sfondo login

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -30,27 +30,27 @@ async function submit() {
 </script>
 
 <template>
-  <div class="h-screen overflow-scroll pb-48 md:pb-0 flex items-center justify-center">
+  <div class="h-screen overflow-scroll pb-48 md:pb-0 flex items-center justify-center bg-[url('/images/wallpaper.png')] bg-cover bg-no-repeat bg-center">
     <Transition name="fade">
-      <form class="w-100 mx-1 md:mx-3">
-        <h1 class="text-xl my-4">Login</h1>
+      <form class="w-96 mx-1 md:mx-3 bg-black/50 border border-black shadow-xl rounded-xl  p-12">
+        <h1 class="text-xl mb-8 text-center text-white uppercase">Login</h1>
         <div class="flex flex-col gap-4">
-          <input class="py-2 px-4"
+          <input class="p-4 text-base"
                  @keydown.enter="submit"
                  type="text" name="username" placeholder="Username" v-model="username">
-          <span class="flex items-center gap-2">
-            <input class="py-2 px-4"
+          <span class="flex items-center gap-3">
+            <input class="p-4 text-base flex-1"
                    autocomplete="off"
                    @keydown.enter="submit"
                    :type="showPassword ? 'text' : 'password'" name="password" placeholder="Password" v-model="password">
-            <Eye class="cursor-pointer" :szie="10" v-if="!showPassword" @click="showPassword = !showPassword"/>
-            <EyeOff class="cursor-pointer" :szie="10"  v-else @click="showPassword = !showPassword"/>
+            <Eye class="cursor-pointer text-white" :size="18" v-if="!showPassword" @click="showPassword = !showPassword"/>
+            <EyeOff class="cursor-pointer text-white" :size="18"  v-else @click="showPassword = !showPassword"/>
           </span>
-          <span class="text-red-400">
+          <span class="bg-red-400 text-white text-center rounded-xl">
             {{ errors }}
           </span>
           <button type="button" @click="submit" :disabled="!username || !password"
-                  class="bg-blue-200 py-2 px-4 rounded-xl disabled:bg-gray-200 disabled:text-gray-400">Accedi
+                  class="bg-green-400 py-2 px-4 rounded-xl disabled:bg-gray-100 disabled:text-gray-400">Accedi
           </button>
         </div>
       </form>

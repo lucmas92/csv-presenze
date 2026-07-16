db1718278411c5ea9ef63d02c7c8b0ada264fb9a
Luca Massignani <luca.massignani@sanmarcoinformatica.it>
2026-07-16 13:53:16 +0200

    rimosso bordo da icona widget

--- app/components/Widget.vue
+++ app/components/Widget.vue
@@ -12,7 +12,7 @@ defineProps({
 </script>
 <template>
   <div class="flex gap-4 border border-gray-300 rounded-xl p-3">
-    <div class="icon px-4 border border-gray-300 flex items-center rounded-xl">
+    <div class="icon px-4  flex items-center">
       <slot/>
     </div>
     <div class="flex flex-col">
